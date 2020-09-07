7691def367abc26a2b0b7c1ac7ee18ffacc1cdc2
Royal Paschall <royal.paschall@gmail.com>
2020-09-07 15:28:00 -0400

    fix card const

--- scripts/Card.js
+++ scripts/Card.js
@@ -84,12 +84,17 @@ _handleImageDisplay(){
 createCard(){
     
     const cardElement = this._getCardTemplate(); //clones all children(img,button,title, etc) under the parent (.card)
-    const cardImageSelector = this._card.querySelector('.card__img');
     this._card = cardElement;
+    
+    const cardImage = 
+        this._card
+        .querySelector('.card__img');
+        
+    
 
     this._card.querySelector('.card__title').textContent = this._name; //places the name, from the data, as the text content of the Card Title class and 
-    cardImageSelector.src = this._link; //places the data from link as the src or source for the image
-    cardImageSelector.alt = this._name; //places the name as Alt for accesibility
+    cardImage.src = this._link; //places the data from link as the src or source for the image
+    cardImage.alt = this._name; //places the name as Alt for accesibility
     
 this._addEventListener();
  
